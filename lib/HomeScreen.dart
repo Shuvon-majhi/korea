1d0b5bf9adc1fe7c12e4ec455ae1339891548f04
Shuvon-majhi <shuvon.majhi@gmail.com>
2024-01-15 23:38:41 +0600

    update

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:korea/data.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  Widget myNewsCard(int index) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+            DataSources.myNews[index]['image'],
+            height: 80,
+          ),
+          Text(
+            DataSources.myNews[index]['headline'],
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Feed'),
+        backgroundColor: Colors.blue,
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return orientation == Orientation.landscape
+              ? GridView.builder(
+                  itemCount: DataSources.myNews.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) => myNewsCard(index),
+                )
+              : ListView.builder(
+                  itemCount: DataSources.myNews.length,
+                  itemBuilder: (context, index) => myNewsCard(index),
+                );
+        },
+      ),
+    );
+  }
+}
